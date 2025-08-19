d56b247ee7b86b72e1043e0744054655a5509c24
Tolu <tolu.nuell@gmail.com>
2025-08-19 11:12:23 +0100

    Added the Get Involved section

---
+++ src/app/components/GetInvolved.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "@/styles/GetInvolved.css";
+
+export const GetInvolved = () => {
+  return (
+    <section id='getInvolved' className='getInvolved'>
+        <div className="container">
+            get involved
+        </div>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import styles from "./page.module.css";
 import { Hero } from "./components/Hero";
 import Navbar from "./components/navBar";
 import { About } from "./components/About";
+import { GetInvolved } from "./components/GetInvolved";
 
 export default function Home() {
   return (
     <>
     <Hero />
     <About />
+    <GetInvolved />
     </>
   );
 }
